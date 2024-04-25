57e09ad937b4945841b4c74a0c21d392084c15e9
alvaro-moschella <alvaro.moschella@gmail.com>
2024-04-25 17:51:47 -0300

    fixed realtimeproducts

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -46,14 +46,4 @@ router.put('/:pid', async (req, res) => {
     }
 });
 
-router.delete('/:pid', async (req, res) => {
-    const { pid } = req.params;
-    try {
-      const deletedProduct = await productManager.deleteProduct(pid);
-      res.status(204).json(deletedProduct);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
-
 export default router
