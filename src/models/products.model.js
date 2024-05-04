afcd07205c19a10e2d9d18b2d5cc2ff68d392258
alvaro-moschella <alvaro.moschella@gmail.com>
2024-05-04 17:52:36 -0300

    products model

---
+++ src/models/products.model.js
@@ -0,0 +1,42 @@
+import { Schema, model } from 'mongoose';
+
+const productsSchema = new Schema({
+    title: { 
+        type: String, 
+        required: true 
+    },
+    description: { 
+        type: String, 
+        required: true 
+    },
+    code: { 
+        type: String, 
+        required: true 
+    },
+    price: { 
+        type: Number, 
+        required: true 
+    },
+    status: { 
+        type: Boolean, 
+        required: true 
+    },
+    stock: { 
+        type: Number, 
+        required: true 
+    },
+    category: { 
+        type: String, 
+        required: true 
+    },
+    thumbnails: { 
+        type: [String], 
+        required: false 
+    }
+})
+
+const productsModel = model('products', productsSchema)
+
+module.exports = {
+    productsModel
+}
