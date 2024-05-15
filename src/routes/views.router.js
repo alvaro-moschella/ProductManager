9410f8143a3af4474ce73638c082f4be4d9bd4d2
alvaro-moschella <alvaro.moschella@gmail.com>
2024-05-15 20:42:38 -0300

    added views to views router

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -2,8 +2,11 @@ import { Router } from 'express'
 import ProductManager from '../dao/ProductManagerFS.js'
 import { productListUpdated } from '../socket.js'
 const router = Router()
-import { __dirname } from '../utils.js'
+import { __dirname, buildPaginatedResponse } from '../utils.js'
+import ProductsManagerMongo from '../dao/productsManagerMongo.js'
+import CartManagerMongo from '../dao/cartsManagerMongo.js'
 
+const cartService = new CartManagerMongo()
 const productManager = new ProductManager('productos.json')
 
 router.get('/', async (req, res) => {
@@ -30,5 +33,43 @@ router.get('/chat', (req, res) => {
   res.render('index', {})
 })
 
+const productService = new ProductsManagerMongo()
+router.get('/products', async (req, res) => {
+  const { limit = 10, page = 1, sort, query } = req.query
+  const criteria = {}
+  const options = { limit, page }
+  if (sort && (sort.toLowerCase() === 'asc' || sort.toLowerCase() === 'desc')) {
+    options.sort = { price: sort }
+  }
+  if (query) {
+    if (query.toLowerCase() === 'true' || query.toLowerCase() == 'false') {
+      criteria.status = query
+    } else {
+      criteria.category = query
+    }
+  }
+    try {
+        const result = await productService.getProducts(criteria, options)
+        const products = buildPaginatedResponse(result, sort, query)
+        res.render('products', products)
+    } catch (error) {
+        res.status(500).send({ status: 'error', error: error.message })
+    }
+})
 
+router.get('carts/:cid', async (req, res) => {
+  const { cid } = req.params
+  try {
+    const cart = await cartService.getCartBy({_id: cid})
+    console.log('carrito', cart)
+    if (!cart) {
+        res.status(404).send({ error: "El carrito no existe" })
+    } else {
+        const cartProducts = { products: cart.products }
+        res.status(200).send(cartProducts)
+    }
+  } catch (error) {
+    res.status(500).send({ error: error.message })
+  }
+})
 export default router
