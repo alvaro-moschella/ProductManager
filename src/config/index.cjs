bb3b0aefa1a6577ab73c96e47826a7d8809d0718
alvaro-moschella <alvaro.moschella@gmail.com>
2024-05-27 18:13:52 -0300

    hide db credentials

--- src/config/index.cjs
+++ src/config/index.cjs
@@ -2,8 +2,7 @@ const { connect } = require('mongoose')
 
 exports.connectDB = () => {
     try {
-        // connect('mongodb://127.0.0.1:27017/c53145')
-        connect('mongodb+srv://admin:tNYclI4wo1nqG1Ge@cluster0.akju9kh.mongodb.net/ecommerce')
+        connect('mongodb://127.0.0.1:27017/c53145')
         console.log('Database connected succesfuly')
     } catch (error) {
         console.error('Ha ocurrido un error al intentar conectarnos a la DB', error.message)
