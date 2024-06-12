7dd05aca23441becc52cc1b8815e4845c04a80d4
alvaro-moschella <alvaro.moschella@gmail.com>
2024-06-12 18:21:35 -0300

    mongo url in env vars

--- src/config/index.cjs
+++ src/config/index.cjs
@@ -1,8 +1,11 @@
 const { connect } = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 exports.connectDB = () => {
     try {
-        connect('mongodb://127.0.0.1:27017/c53145')
+        connect(process.env.MONGO_URL_ATLAS)
         console.log('Database connected succesfuly')
     } catch (error) {
         console.error('Ha ocurrido un error al intentar conectarnos a la DB', error.message)
